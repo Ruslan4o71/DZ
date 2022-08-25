7dcc4480f061a5033dda6f0880e7c6d6a507e039
Ruslan Ignatov <diztorgcentr@yandex.ru>
2022-08-25 18:34:17 +0300

    Сохранение перед отправкой

---
+++ lesson_2/№10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+// метод int
+
+Console.WriteLine("Введите трехзначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра { number1 / 10 % 10}");
+
+
+
+// метод string
+
+//Console.WriteLine("Введите трехзначное число: ");
+//string number1 = Console.ReadLine();
+//Console.WriteLine($"Вторая цифра {Convert.ToInt32(new string(number1[1], 1))}");
---
+++ lesson_2/№13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+string number1 = Console.ReadLine();
+if(number1.Length < 3)
+{
+Console.WriteLine("Третья цифра отсутствует");
+}
+else 
+{
+Console.WriteLine($"Третья цифра {Convert.ToInt32(new string(number1[2], 1))}");
+}
